e5a70601336991946b663ed038385c141140fd90
Matt Travi <programmer@travi.org>
2013-07-07 22:07:38 -0500

    simplified hierarchy

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+/*global module*/
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-buster');
+    grunt.loadNpmTasks('grunt-jstestdriver');
+
+    // Project configuration.
+    grunt.initConfig({
+        clean: ['client/css'],
+
+        jslint: {
+            files: [
+                'grunt.js',
+                'js/**/*.js',
+                'test/**/*.js',
+                '!test/tools/*.js',
+                '!test/stubs/*.js',
+                '!test/resources/bootstrap.js'
+            ],
+            directives: {
+                browser: true,
+                predef: [
+                    '$',
+                    'jQuery',
+                    'Modernizr',
+                    'amplify',
+                    'travi',
+
+                    'sinon',
+                    'assertEquals',
+                    'assertSame',
+                    'assertNotSame',
+                    'assertString',
+                    'assertObject',
+                    'assertFunction',
+                    'assert',
+                    'refute',
+                    'assertFalse',
+                    'assertTrue',
+                    'expectAsserts'
+                ]
+            },
+            options: {
+                errorsOnly: true,
+                checkstyle: 'logs/jslint.xml'
+            }
+        },
+
+        buster: {
+            dist: {
+                server: {
+                    port: 1111
+                }
+            }
+        },
+
+        jstestdriver: {
+            files: [
+                'jsTestDriver.conf'
+            ]
+        }
+    });
+
+    // Default task.
+    grunt.registerTask('default', 'jslint');
+    grunt.registerTask('gates', ['jslint', 'jstestdriver']);
+};
