99c40d95079e6a02bffce42c2700b19527506453
Matt Travi <programmer@travi.org>
2013-11-18 07:15:18 -0600

    clean up test file

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,56 +70,16 @@ module.exports = function (grunt) {
             }
         },
 
-        buster: {
-            dist: {
-                server: {
-                    port: 1111
-                }
-            }
-        },
-
-        jstestdriver: {
-            files: [
-                'jsTestDriver.conf'
-            ]
-        },
-
         karma: {
             unit: {
-                options: {
-                    files: [
-                        {pattern: 'templates/**/*.tmpl', included: false},
-                        {pattern: 'test/templates/**/*.tmpl', included: false},
-
-                        'node_modules/karma-jstd-adapter/jstd-adapter.js',
-                        'bower_components/jquery/jquery.js',
-                        'bower_components/jquery-ui/ui/jquery-ui.js',
-                        'bower_components/jsrender/jsrender.js',
-                        'bower_components/travi-test-utils/stubs/amplifyStub.js',
-                        'bower_components/travi-test-utils/stubs/modernizrStub.js',
-                        'bower_components/travi-test-utils/stubs/momentStub.js',
-
-                        'js/travi.js',
-                        'bower_components/travi-test-utils/tools/sinon.js',
-                        'bower_components/travi-test-utils/tools/*.js',
-                        'js/travi/templates.js',
-                        'test/resources/load-templates.js',
-                        'js/travi/location.js',
-                        'js/travi/events.js',
-                        'js/travi/pagination.js',
-                        'bower_components/travi-test-utils/test-init.js',
-                        'js/**/*.js',
-
-                        'bower_components/travi-test-utils/stubs/ajaxStub.js',
-                        'test/**/*.js'
-                    ]
-                },
-                browsers: ['PhantomJS'],
+                configFile: 'karma.conf.js',
                 singleRun: true,
+
                 reporters: [
                     'progress',
                     'junit'
                 ],
+
                 junitReporter: {
                     outputFile: 'logs/karma.xml'
                 }
