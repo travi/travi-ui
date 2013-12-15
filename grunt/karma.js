b1e64c21b72ab95a01b3479b0433b6ba9efce6bb
Matt Travi <programmer@travi.org>
2013-12-15 16:01:22 -0600

    added coverage with the hope that i can get it sucked into coveralls

--- grunt/karma.js
+++ grunt/karma.js
@@ -3,13 +3,23 @@ module.exports = {
         configFile: 'karma.conf.js',
         singleRun: true,
 
+        preprocessors: {
+            "js/**/*.js": "coverage"
+        },
+
         reporters: [
             'progress',
-            'junit'
+            'junit',
+            'coverage'
         ],
 
         junitReporter: {
             outputFile: 'logs/karma.xml'
+        },
+
+        coverageReporter: {
+            type: "lcov",
+            dir: 'logs/coverage/'
         }
     }
 };
