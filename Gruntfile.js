7379734a54c7c4f397280c44dc99c1472bf364e2
Matt Travi <programmer@travi.org>
2014-01-25 12:37:56 -0600

    added alias file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,4 @@ module.exports = function (grunt) {
     'use strict';
 
     require('load-grunt-config')(grunt);
-
-    grunt.registerTask('gates', ['jslint', 'karma']);
-    grunt.registerTask('travis', ['gates', 'coveralls']);
-    grunt.registerTask('default', 'gates');
 };
