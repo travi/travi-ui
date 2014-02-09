f8e1f764c767c4cd3f8fa42ae2d2df1ee5ef6d90
Matt Travi <programmer@travi.org>
2014-02-09 15:30:05 -0600

    old grunt ui task now default

--- grunt/jslint.js
+++ grunt/jslint.js
@@ -22,61 +22,6 @@ module.exports = {
                 '$',
                 'jQuery',
                 'Modernizr',
-                'amplify',
-                'travi'
-            ]
-        },
-        options: {
-            errorsOnly: true,
-            checkstyle: 'logs/jslint-dist.xml'
-        }
-    },
-    test: {
-        src: [
-            'test/**/*.js',
-            '!test/tools/*.js',
-            '!test/stubs/*.js',
-            '!test/resources/bootstrap.js'
-        ],
-        directives: {
-            browser: true,
-            unparam: true,
-            predef: [
-                '$',
-                'jQuery',
-                'Modernizr',
-                'amplify',
-                'travi',
-
-                'sinon',
-                'assertEquals',
-                'assertSame',
-                'assertNotSame',
-                'assertString',
-                'assertObject',
-                'assertFunction',
-                'assert',
-                'refute',
-                'assertFalse',
-                'assertTrue',
-                'expectAsserts'
-            ]
-        },
-        options: {
-            errorsOnly: true,
-            checkstyle: 'logs/jslint-test.xml'
-        }
-    },
-    ui: {
-        src: [
-            'js/**/*.js'
-        ],
-        directives: {
-            browser: true,
-            predef: [
-                '$',
-                'jQuery',
-                'Modernizr',
                 'travi'
             ]
         },
@@ -85,7 +30,7 @@ module.exports = {
             checkstyle: 'logs/jslint-ui.xml'
         }
     },
-    uiTest: {
+    test: {
         src: [
             'test/**/*.jstd'
         ],
--- grunt/karma.js
+++ grunt/karma.js
@@ -1,28 +1,5 @@
 module.exports = {
     unit: {
-        configFile: 'karma.conf.js',
-        singleRun: true,
-
-        preprocessors: {
-            "js/**/!(utilities).js": 'coverage'
-        },
-
-        reporters: [
-            'progress',
-            'junit',
-            'coverage'
-        ],
-
-        junitReporter: {
-            outputFile: 'logs/karma.xml'
-        },
-
-        coverageReporter: {
-            type: 'lcov',
-            dir: 'logs/coverage/'
-        }
-    },
-    ui: {
         configFile: 'ui.conf.js',
         singleRun: true,
 
