d977702d9aea465a994a68d767dba7382676fa77
Matt Travi <programmer@travi.org>
2014-04-20 20:56:30 -0500

    initial dialog form tests

---
+++ js/dialog/form.js
@@ -0,0 +1,28 @@
+(function (travi) {
+    'use strict';
+
+    var events = travi.events,
+        dialogEvents = travi.ui.dialog.events;
+
+    function handleFormSubmission(e) {
+        var $form = $(this);
+        e.preventDefault();
+
+        $.ajax({
+            url: $form.attr('action'),
+            type: $form.attr('method')
+        });
+    }
+
+    function enhanceForm(data) {
+        data.$dialog.find('form').submit(handleFormSubmission);
+    }
+
+    function init() {
+        events.subscribe(dialogEvents.keys.LOADED, enhanceForm);
+    }
+
+    travi.register('ui.dialog.form', {
+        init: init
+    });
+}(travi));
