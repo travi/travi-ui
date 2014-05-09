0ba00e747d2347e6f44890eecf39ed11647ef409
Matt Travi <programmer@travi.org>
2014-05-08 21:51:46 -0500

    refactoring

--- js/form/richText.js
+++ js/form/richText.js
@@ -1,15 +1,17 @@
 (function (travi) {
     'use strict';
 
+    function init() {
+        $('textarea.richEditor').wymeditor({
+            skin: 'silver',
+            updateSelector: 'form',
+            postInit: function (wym) {
+                wym.fullscreen();
+            }
+        });
+    }
+
     travi.register('ui.form.richText', {
-        init: function () {
-            $('textarea.richEditor').wymeditor({
-                skin: 'silver',
-                updateSelector: 'form',
-                postInit: function (wym) {
-                    wym.fullscreen();
-                }
-            });
-        }
+        init: init
     });
 }(travi));
