1d93ab8d04cb3f0ca83c8aa69dfa5c2ff9d59bb0
Matt Travi <programmer@travi.org>
2014-06-08 12:18:38 -0500

    used world and proxies from test-utils

--- cucumber.conf.js
+++ cucumber.conf.js
@@ -7,14 +7,14 @@ module.exports = function (config) {
 
         files: [
             'bower_components/amplify/core/amplify.core.js',
-            'test/resources/amplifyProxy.js',
+            'bower_components/travi-test-utils/proxies/amplifyProxy.js',
             'bower_components/jquery/jquery.js',
             'bower_components/jquery-ui/ui/jquery-ui.js',
             'bower_components/jquery-migrate/jquery-migrate.js',
             'bower_components/wymeditor/dist/wymeditor/jquery.wymeditor.js',
 
             'bower_components/travi-core/js/travi.js',
-            'test/resources/registerProxy.js',
+            'bower_components/travi-test-utils/proxies/registerProxy.js',
             'bower_components/travi-core/js/travi/dependencies/checker.js',
             'bower_components/travi-core/js/travi/events.js',
 
@@ -32,7 +32,7 @@ module.exports = function (config) {
             {pattern: 'test/integration/features/**/*.feature', watched: true, included: false, served: true},
 
             'bower_components/travi-test-utils/tools/common.js',
-            'test/integration/world.js',
+            'bower_components/travi-test-utils/tools/cucumber-world.js',
             {pattern: 'test/integration/features/step_definitions/**/*.js', watched: true, included: true, served: true}
         ],
 
