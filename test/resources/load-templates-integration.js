1f804c10c2432db857844d4eb0875969b1aa3016
Matt Travi <programmer@travi.org>
2014-06-22 11:06:34 -0500

    a minor cleanup step for using templates in integration tests. still havent figured out the proper path #28

--- test/resources/load-templates-integration.js
+++ test/resources/load-templates-integration.js
@@ -1,5 +1,3 @@
 travi.templates.init();
 
-travi.templates.preLoad('update-item', '/test/templates/stub-update-item.tmpl');
-travi.templates.preLoad('entityList', '/test/templates/entity-list.tmpl');
-travi.templates.preLoad('pagination', '/test/templates/pagination.tmpl');
+travi.templates.preLoad('entityList', '../templates/entity-list.tmpl');
