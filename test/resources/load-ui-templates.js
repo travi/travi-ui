7827a15f18320c5013b764e4e463d7a4d516f46c
Matt Travi <github@travi.org>
2014-12-03 14:08:37 -0600

    stub preLoad after loading templates to prevent server-calls from modules

--- test/resources/load-ui-templates.js
+++ test/resources/load-ui-templates.js
@@ -3,3 +3,5 @@ travi.templates.init();
 travi.templates.preLoad('update-item', '/base/test/templates/stub-update-item.tmpl');
 travi.templates.preLoad('entityList', '/base/test/templates/entity-list.tmpl');
 travi.templates.preLoad('pagination', '/base/test/templates/pagination.tmpl');
+
+travi.tempaltes.preLoad = function () {};
