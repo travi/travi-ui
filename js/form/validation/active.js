b6614c57284cdefd385a460728964b6dd0b9d5ab
Matt Travi <programmer@travi.org>
2015-01-16 22:00:55 -0600

    placed the error message after the choices list to close travi/styles#14

--- js/form/validation/active.js
+++ js/form/validation/active.js
@@ -1,9 +1,20 @@
 (function (travi) {
     'use strict';
 
+    function placeMessage($error, $element) {
+        var $parents = $element.parents('.choices');
+
+        if (0 < $parents.length) {
+            $parents.after($error);
+        } else {
+            $element.after($error);
+        }
+    }
+
     function init() {
         $('form').validate({
-            errorClass: 'ui-state-error'
+            errorClass: 'ui-state-error',
+            errorPlacement: placeMessage
         });
     }
 
