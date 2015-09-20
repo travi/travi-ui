371267b363904ddb0d245bb8e349fa829b59ec2b
Matt Travi <programmer@travi.org>
2015-09-19 21:15:47 -0500

    configured load-grunt-config to use jitgrunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,5 +3,7 @@ module.exports = function (grunt) {
     'use strict';
 
     require('time-grunt')(grunt);
-    require('load-grunt-config')(grunt);
+    require('load-grunt-config')(grunt, {
+        jitGrunt: {}
+    });
 };
